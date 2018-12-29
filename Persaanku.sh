2de70be35bcef9193ca1a1b1617f9371bd2e5bed
/Z3X_ID <46043473+rianda456@users.noreply.github.com>
2018-12-29 09:42:13 +0700

    Add files via upload

---
+++ Persaanku.sh
@@ -0,0 +1,70 @@
+
+#!/system/xbin/bash
+#MauNgapainGblk?
+#DiRecodeGakBakalJadiinLuMastah
+#DiGantiAuthorGakBakalJadiinLuMaster
+#MikirMakeOtakGblk
+#KontolKaloMauReedit/Recode
+#Pm Gua Bangsat
+clear
+cd module
+python2 please.py
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo $red
+cat asw.txt
+echo ""
+echo "\033[34;1m[$]\033[36;1m============================\033[34;1m[$]"
+echo "\033[32;1mAuthor : ./Z3X_ID"
+echo "\033[37;1mFeat: TENGKORAK HACKER TEAM"
+echo "\033[35;1mTeam : BOJONEGORO CYBER SECURITY"
+echo "\033[33;1mKontak Me :  085816406954"
+echo "\033[33;1mThank´s To : BEBLLY OKTAVIANI" 
+echo "\033[92mBOJONEGORO CYBER SECURITY"
+echo "\033[36;1mKami Bukan Sampah Masyarakat"
+echo "\033[34;1m[$]\033[36;1m============================\033[34;1m[$]"
+echo ""
+echo "\033[32;1mSilahkan Pilih Tools Yang mau Kamu pake:)?:"
+echo "\033[36;1m"
+echo "[===============================================[>"
+echo $red "1.> YES "
+echo "[===============================================]>"
+echo $blue "2.> NO "
+echo "[===============================================]>"
+echo "\033[32;1m"
+read -p "root@tools-./Z3X_ID ft Beblly~#" bro
+[ $bro = 24 ] || [ $bro = 24 ]
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f standard -F gay "./Z3X_ID"
+sleep 3
+echo "\033[33:1mMakasih ^_^"
+echo "\033[32:1mTelah Menerimaku Dengan Ikhlas >//<"
+echo "\033[32:1mAku Gak Bisa Apa Apa >_< ,Aku Hanyalah Seorang"
+echo "\033[34:1mPROGRAMMER BIASA"
+echo "\033[32:1mHanya Membuat Kode Kode Program"
+echo "\033[32:1mKode Cewek Yang Tak Bisa Kupahami"
+echo "\033[32:1mAku Hanyalah Seseorang Yg Cupu :'("
+echo "\033[32:1mSeseorang Yg Lemah :")"
+echo "\033[33:1mMakasih Ya ^_^"
+echo "\033[34:1mI LOVE YOU :*"
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet -f standard -F gay "./Z3X_ID"
+echo "\033[34:1mOk Aku Mengerti Kok Perasaanmu"
+echo "\033[34:1mMungkin Aku Tak Cocok Bagimu"
+echo "\033[34:1mTerima Kasih ^_^"
+echo "\033[34:1mTelah Menemaniku"
+echo "\033[31:1mI LOVE YOU"
+fi
